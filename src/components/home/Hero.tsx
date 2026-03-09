54aca9e40efe3abdaa2871dac04138b2cfc8772b
davidfayanju1 <davidfayanju01@gmail.com>
2026-03-09 07:59:47 +0000

    UI home page

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -216,7 +216,7 @@ const Hero = () => {
 
       {/* Event Modal - Mobile Optimized */}
       {showEventModal && (
-        <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
+        <div className="fixed inset-0 z-[100000] flex items-center justify-center p-4">
           {/* Backdrop */}
           <div
             className="absolute inset-0 bg-black/70 backdrop-blur-sm"
